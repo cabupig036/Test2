9cafdc30a578efc834b363956fb58c71f5617ad9
Thanhthuan020 <thanhthuan019@gmail.com>
2022-07-28 08:13:45 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,14 +3,9 @@ const UserSchema = new mongoose.Schema({
     gmailUser: String,
     passwordHash : String,
     addressUser : String,
-    isVipMember : String,
     phoneUser : String,
     Username :  String,
     profilePictureUser : String,
-    Discount50 : String,
-    Discount30 : String,
-    PointVIP : String,
-    BlackList : String,
     id : String
 },);
 
