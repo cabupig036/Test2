1b15c97a353c9a45693c4a2f89650ca3a4623c38
Thanhthuan020 <thanhthuan019@gmail.com>
2022-08-09 12:45:14 +0700

    x

--- routes/cod.js
+++ routes/cod.js
@@ -167,10 +167,10 @@ router.put("/acceptCOD/:_id", async (req, res) => {
         cod.time.push(timeCOD);
         cod.month = monthCOD;
         cod.status = "Đã COD",
-        cod.priceCOD = (cod.priceCOD - req.body.money - 5000)
+        cod.price = (cod.priceCOD - money) 
 
         cod.save();
-        return res.status(200).json(cod.priceCOD, cod.totalCollectionMoney, "5000");
+        return res.status(200).json(cod.price);
       }
       else{
         res.status(404).json({
